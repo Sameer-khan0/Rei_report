d5c1bfc1738a5b2d22e7bd606c9cbca71b0e8a9a
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2025-01-06 16:25:30 +0500

    update

--- src/component/page2Content.jsx
+++ src/component/page2Content.jsx
@@ -3,8 +3,9 @@ import "./page2.css"
 
 export default function page2Content() {
   return (
-    <div className="container">
-      page2.Content
+    <div className="page2container" style={{height:"100vh"}}>
+      <div className="page2leftside"></div>
+      <div className="page2rightside"></div>
     </div>
   )
 }
